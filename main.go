ea9ad97fe4595585b8f5e57cb731cff02f522ab0
Patrick Negri <patrick@iugu.com>
2023-07-08 00:56:11 -0300

    upgrading to 4

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const INDEX = `<!DOCTYPE html>
     <title>GitOps Testing</title>
   </head>
   <body>
-	Sample Go App - Version 3
+	Sample Go App - Version 4
   </body>
 </html>`
 
@@ -28,7 +28,7 @@ func getenv(key, fallback string) string {
 }
 
 func main() {
-	fmt.Println("Version 3")
+	fmt.Println("Version 4")
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintln(w, INDEX)
