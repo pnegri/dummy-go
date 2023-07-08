c0b5dc08145bbd579040a2f6c93fec3735f46c88
Patrick Negri <patrick@iugu.com>
2023-07-07 23:16:35 -0300

    version 1

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+const INDEX = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>GitOps Testing</title>
+  </head>
+  <body>
+	Sample Go App - Version 1
+  </body>
+</html>`
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(w, INDEX)
+	})
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+}
